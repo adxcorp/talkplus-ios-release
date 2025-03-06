4a0ae55a124b090994c005cc6a2972391ac08a69
JCLEE <dlwocjs1618@gmail.com>
2025-03-06 16:36:48 +0900

    [수정] Klat SDK (TalkPlus SDK) 1.0.2 적용

--- ios/TalkPlus.xcframework/ios-arm64_x86_64-simulator/TalkPlus.framework/Headers/TalkPlus.h
+++ ios/TalkPlus.xcframework/ios-arm64_x86_64-simulator/TalkPlus.framework/Headers/TalkPlus.h
@@ -20,7 +20,7 @@
 #import "TPChannelUpdateParams.h"
 #import "TalkPlusLog.h"
 
-#define TALKPLUS_SDK_VERSION @"1.0.1"
+#define TALKPLUS_SDK_VERSION @"1.0.2"
 
 @protocol TPChannelDelegate <NSObject>
 @required
